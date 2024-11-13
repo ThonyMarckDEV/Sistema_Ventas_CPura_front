a60adf1e397d118053778f08932a989e97e0c605
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-11-13 16:00:24 -0500

    aea

--- js/checkStorageTokenINDEX.js
+++ js/checkStorageTokenINDEX.js
@@ -1,5 +1,3 @@
-import API_BASE_URL from './urlHelper.js';
-
 // Agregar un evento para verificar el token almacenado al cargar la página
 document.addEventListener("DOMContentLoaded", checkStoredToken);
 
